423061561a7166a280920364f4bd15d4633644e6
jiangyu <jiangyuwise@gmail.com>
2019-09-19 09:49:05 +0800

    优化依赖

--- app/src/main/java/com/codve/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/codve/criminalintent/CrimeListFragment.java
@@ -1,8 +1,6 @@
 package com.codve.criminalintent;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
